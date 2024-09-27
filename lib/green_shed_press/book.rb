4c41f08c7858cf054b2010e30639456fc0e7e1db
Jeff McFadden <jeff@mcfadden.io>
2024-09-26 19:12:09 -0700

    Book layout.

--- lib/green_shed_press/book.rb
+++ lib/green_shed_press/book.rb
@@ -4,6 +4,8 @@ module GSP
 
     def_delegators :frontmatter, :title, :author, :series, :series_position, :pages, :rating, :started_reading_at, :finished_reading_at, :asin
 
+    def layout; "book"; end
+
     def output_filepath
       # Replace any non a-z, 0-9, or _ with nothing
       self.frontmatter.slug || "/bookshelf/#{self.title.downcase.gsub(" ", "_").gsub(/[^a-z0-9_]/i, '')}.html"
